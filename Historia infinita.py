ef6ca2d7fff5a180abd989701c58aca1bc9ed4b9
edgarsincel <63528619+edgarsincel@users.noreply.github.com>
2023-03-01 23:13:36 -0600

    Add files via upload

---
+++ Historia infinita.py
@@ -0,0 +1,7 @@
+name = input("¿Cómo te llamas? ")
+age = int(input("¿Qué edad tienes? "))
+food = input ("¿Qué te gusta comer? ")
+future_age = age + 10
+
+print("Hola " + name + "! En 10 años, tendrás " + str(future_age) + " años, y comerás " + food + " todo el día.")
+
